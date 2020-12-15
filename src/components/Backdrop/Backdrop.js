b45157d94aa3faf08dc54e4b6be33c62d797569e
shrey9496 <shrey9april@gmail.com>
2020-12-15 16:07:13 +0530

    Added Backdrop component and refactored navigation

---
+++ src/components/Backdrop/Backdrop.js
@@ -0,0 +1,5 @@
+const Backdrop = ({ click }) => (
+   <div className="backdrop" onClick={click} />
+)
+
+export default Backdrop
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import '../styles/styles.scss'
+import '../styles/main.scss'
 
 const MyApp = ({ Component, pageProps }) => {
   return <Component {...pageProps} />
