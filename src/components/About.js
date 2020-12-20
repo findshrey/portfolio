2c9b484c4229a163317c29623e5dd838617e7ec7
shrey9496 <shrey9april@gmail.com>
2020-12-20 19:55:57 +0530

    Added About component

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,24 @@
 const About = () => {
    return (
       <section className="about">
-         yoyoyo
+         <div className="container">
+            <header className="head-white">
+               <h3>About Me</h3>
+            </header>
+            <div className="about-inner">
+               <div className="about-image">
+                  <img src="/about.svg" alt="" />
+               </div>
+               <div className="about-content">
+                  <p>
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dicta consequuntur laborum error voluptas quos odit suscipit pariatur aut tempora!
+                  </p>
+                  <p>
+                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptatum pariatur reiciendis amet quam eveniet nesciunt, sit ducimus eos assumenda.
+                  </p>
+               </div>
+            </div>
+         </div>
       </section>
    )
 }
