85a23bf5033bdf84f793b24eb273451369bba6e0
shrey9496 <shrey9april@gmail.com>
2020-12-21 21:08:29 +0530

    Added Experience component

---
+++ src/components/Experience.js
@@ -0,0 +1,35 @@
+const Experience = () => {
+   return (
+      <section className="experience">
+         <div className="container">
+            <header className="head-black">
+               <h3>Where I've Worked</h3>
+            </header>
+            <div className="timeline">
+               <ul className="work">
+                  <li>
+                     <h4>2010-2012</h4>
+                     <p>delhi</p>
+                     <p>student</p>
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, delectus.</p>
+                  </li>
+                  <li>
+                     <h4>2010-2012</h4>
+                     <p>delhi</p>
+                     <p>student</p>
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, delectus.</p>
+                  </li>
+                  <li>
+                     <h4>2010-2012</h4>
+                     <p>delhi</p>
+                     <p>student</p>
+                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, delectus.</p>
+                  </li>
+               </ul>
+            </div>
+         </div>
+      </section>
+   )
+}
+
+export default Experience
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,11 @@
 import Header from './../components/Header'
-import Hero from './../components/Hero'
-import Projects from './../components/Projects'
-import Divider from './../components/utilities/divider'
-import About from './../components/About'
-import Contact from './../components/Contact'
-import Footer from './../components/Footer'
+import Hero from '../components/Hero'
+import Projects from '../components/Projects'
+import Divider from '../components/utilities/divider'
+import About from '../components/About'
+import Contact from '../components/Contact'
+import Footer from '../components/Footer'
+import Experience from '../components/Experience'
 
 const PortfolioApp = () => {
   return (
@@ -15,6 +16,7 @@ const PortfolioApp = () => {
         <Projects />
         <Divider />
         <About />
+        <Experience />
         <Contact />
       </main>
       <Footer />
