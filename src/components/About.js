1eb048c7d98185d019099ab514efa35afb331d5e
shrey9496 <shrey9april@gmail.com>
2020-12-24 03:20:10 +0530

    Refactor: Added animation to About section and other minor changes.

--- src/components/About.js
+++ src/components/About.js
@@ -1,26 +1,39 @@
-const About = () => {
-   return (
-      <section className="about">
-         <div className="container">
-            <header className="head-white">
-               <h3>About Me</h3>
-            </header>
-            <div className="about-inner">
-               <div className="about-image">
-                  <img src="/about.svg" alt="" />
-               </div>
-               <div className="about-content">
-                  <p>
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae dicta consequuntur laborum error voluptas quos odit suscipit pariatur aut tempora!
-                  </p>
-                  <p>
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa voluptatum pariatur reiciendis amet quam eveniet nesciunt, sit ducimus eos assumenda.
-                  </p>
-               </div>
+import svgDev from '../data/svg-dev'
+
+const About = () => (
+   <section id="about" className="about">
+      <div className="container">
+         <header className="head-white">
+            <h3>About Me</h3>
+         </header>
+         <div className="about-inner">
+            <div className="about-image">
+               {svgDev}
+            </div>
+            <div className="about-content">
+               <p>
+                  Hello! I'm Shrey, a software engineer based in Delhi, India.
+               </p>
+               <p>
+                  I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. I'm always ready to challenge myself and explore new areas of work.
+               </p>
+               <p>
+                  Well-organised person, problem solver with high attention to detail. My goal is to always build products that provide pixel-perfect, performant experiences.
+               </p>
+               <p>
+                  Here are a few technologies I've been working with recently:
+               </p>
+               <ul className="skillset">
+                  <li>HTML & (S)CSS</li>
+                  <li>JavaScript (ES6+)</li>
+                  <li>React</li>
+                  <li>Next.js</li>
+                  <li>Figma</li>
+               </ul>
             </div>
          </div>
-      </section>
-   )
-}
+      </div>
+   </section>
+)
 
 export default About
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,31 +1,29 @@
 import svgProfessional from '../data/svg-professional'
 
-const Hero = () => {
-   return (
-      <section id="home" className="hero">
-         <div className="container">
-            <div className="hero-inner">
-               <div className="hero-text">
-                  <span>Hi, my name is</span>
-                  <h1>Shrey Sharma.</h1>
-                  <h2>I build things for the web.</h2>
-                  <p>
-                     I'm a software engineer based in Delhi, specializing in
-                     building (and occasionally designing) exceptional websites,
-                     applications, and everything in between.
-                  </p>
-                  <div className="call-to-action">
-                     <a href="" className="btn-pink">Projects</a>
-                     <a href="" className="btn-pink">Contact</a>
-                  </div>
+const Hero = () => (
+   <section id="home" className="hero">
+      <div className="container">
+         <div className="hero-inner">
+            <div className="hero-text">
+               <span>Hi, my name is</span>
+               <h1>Shrey Sharma.</h1>
+               <h2>I build things for the web.</h2>
+               <p>
+                  I'm a software engineer based in Delhi, specializing in
+                  building (and occasionally designing) exceptional websites,
+                  applications, and everything in between.
+               </p>
+               <div className="call-to-action">
+                  <a href="" className="btn-pink">Projects</a>
+                  <a href="" className="btn-pink">Contact</a>
                </div>
-               {
-                  svgProfessional
-               }
             </div>
+            {
+               svgProfessional
+            }
          </div>
-      </section>
-   )
-}
+      </div>
+   </section>
+)
 
 export default Hero
