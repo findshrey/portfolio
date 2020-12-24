0060f1e30d1932205cd1bc569907a6d27ce364be
shrey9496 <shrey9april@gmail.com>
2020-12-24 22:30:20 +0530

    Refactor: Adjusted Styling and contents of Experience section

--- src/components/About.js
+++ src/components/About.js
@@ -18,7 +18,7 @@ const About = () => (
                   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between. I'm always ready to challenge myself and explore new areas of work.
                </p>
                <p>
-                  Well-organised person, problem solver with high attention to detail. My goal is to always build products that provide pixel-perfect, performant experiences.
+                  The main area of my expertise is front end development. My goal is to always build products that provide pixel-perfect, performant experiences.
                </p>
                <p>
                   Here are a few technologies I've been working with recently:
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,74 +2,86 @@ import * as FaIcons from 'react-icons/fa'
 
 const Experience = () => {
    return (
-      <section className="experience">
+      <section id="experience" className="experience">
          <div className="container">
             <header className="head-black">
-               <h3>Where I've Worked</h3>
+               <h3>My Experience</h3>
             </header>
-            <div className="experience-wrapper">
-               <div className="work">
+            <div className="timeline-wrapper">
+               <div className="timeline">
                   <div className="timeline-title">
                      <FaIcons.FaBookmark className="title-icon" />
                      <h4>Work History</h4>
                   </div>
                   <ul className="timeline-content">
                      <li>
-                        <h5 className="timeline-date">2010-2012</h5>
+                        <span className="timeline-date">Oct '19 - Mar '20</span>
                         <div className="timeline-info">
-                           <p>Company/ School name</p>
-                           <p>position</p>
-                           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, delectus.</p>
+                           <div className="timeline-info-head">
+                              <h5>Delhi University (IIC)</h5>
+                              <p>Full Stack Developer</p>
+                           </div>
+                           <p className="timeline-info-details">
+                              Worked on MHRD's Project-SAMARTH.
+                              Developed various modules in a vast Central University system based ERP called UIMS
+                              (University Informations Management System) built on PHP (Yii)
+                           </p>
                         </div>
                      </li>
                      <li>
-                        <h5 className="timeline-date">2010-2012</h5>
+                        <span className="timeline-date">Mar '19 - Aug '19</span>
                         <div className="timeline-info">
-                           <p>Company/ School name</p>
-                           <p>position</p>
-                           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, delectus.</p>
+                           <div className="timeline-info-head">
+                              <h5>Sparknet Infotech</h5>
+                              <p>Web Developer</p>
+                           </div>
+                           <p className="timeline-info-details">Developed and maintained code for in-house and client websites primarily using HTML, CSS and JavaScript.</p>
                         </div>
                      </li>
-                     <li>
-                        <h5 className="timeline-date">2010-2012</h5>
+                     {/* <li>
+                        <span className="timeline-date">Jun '17 - Aug '17</span>
                         <div className="timeline-info">
-                           <p>Company/ School name</p>
-                           <p>position</p>
+                           <p>NIIT</p>
+                           <p>Web Developer Intern</p>
                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, delectus.</p>
                         </div>
-                     </li>
+                     </li> */}
                   </ul>
                </div>
-               <div className="education">
+               <div className="timeline">
                   <div className="timeline-title">
                      <FaIcons.FaUserGraduate className="title-icon" />
                      <h4>Education</h4>
                   </div>
                   <ul className="timeline-content">
                      <li>
-                        <h5 className="timeline-date">2010-2012</h5>
+                        <span className="timeline-date">Aug '14 - Jun '18</span>
                         <div className="timeline-info">
-                           <p>Company/ School name</p>
-                           <p>position</p>
-                           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, delectus.</p>
+                           <div className="timeline-info-head">
+                              <h5>Galgotias College of Engineering & Technology</h5>
+                              <p>Computer Science and Engineering</p>
+                           </div>
+                           <p className="timeline-info-details">Computer Science.</p>
                         </div>
                      </li>
                      <li>
-                        <h5 className="timeline-date">2010-2012</h5>
+                        <span className="timeline-date">Jun '12 - Jun '14</span>
                         <div className="timeline-info">
-                           <p>Company/ School name</p>
-                           <p>position</p>
-                           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, delectus.</p>
+                           <div className="timeline-info-head">
+                              <h5>Delhi Public School, Mathura Road</h5>
+                              <p>High School, CBSE</p>
+                           </div>
+                           <p className="timeline-info-details">Computer Science.</p>
                         </div>
                      </li>
-                     <li>
-                        <h5 className="timeline-date">2010-2012</h5>
+                     {/* <li>
+                        <span className="timeline-date">May '10 - Jun '12</span>
                         <div className="timeline-info">
-                           <p>Company/ School name</p>
-                           <p>position</p>
+                           <p>Delhi Public School, Mathura Road</p>
+                           <p>Middle School, CBSE</p>
                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel, delectus.</p>
                         </div>
-                     </li>
+                     </li> */}
                   </ul>
                </div>
             </div>
