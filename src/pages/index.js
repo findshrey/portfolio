001b872d17f6bbc2722f3690208443292a0cfb65
shrey9496 <shrey9april@gmail.com>
2021-03-05 18:13:43 +0530

    Update: Readme

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ const PortfolioApp = () => {
    return (
       <>
          <Head>
-            <meta charset="UTF-8" />
+            <meta charSet="UTF-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <meta
                name="description"
