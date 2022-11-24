5af1b85b20e37654cba940597e4d3ff562141fb0
Shrey Sharma <findshrey@outlook.com>
2022-11-24 22:47:11 +0530

    Refactor: improved semantic tags for hero section

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -3,28 +3,26 @@ import Professional from "../illustrations/Professional"
 const Hero = () => (
    <section id="home" className="hero">
       <div className="container">
-         <div className="hero-inner">
-            <div className="hero-text">
-               <span>Hi, my name is</span>
-               <h1>Shrey Sharma.</h1>
-               <h2>I build things for the web.</h2>
-               <p>
-                  I'm a software engineer based in Delhi, specializing in
-                  Front-end development, looking to facilitate the creation of
-                  modern and aesthetically pleasing websites.
-               </p>
-               <div className="call-to-action">
-                  <a href="#projects" className="btn-pink">
-                     Projects
-                  </a>
-                  <a href="#contact" className="btn-pink">
-                     Contact
-                  </a>
-               </div>
+         <div className="hero-text">
+            <p>Hi, my name is</p>
+            <h1>Shrey Sharma.</h1>
+            <p>I build things for the web.</p>
+            <p>
+               I'm a software engineer based in Delhi, specializing in Front-end
+               development, looking to facilitate the creation of modern and
+               aesthetically pleasing websites.
+            </p>
+            <div className="call-to-action">
+               <a href="#projects" className="btn-pink">
+                  Projects
+               </a>
+               <a href="#contact" className="btn-pink">
+                  Contact
+               </a>
             </div>
-            <div className="hero-img">
-               <Professional />
-            </div>
+         </div>
+         <div className="hero-img">
+            <Professional />
          </div>
       </div>
    </section>
