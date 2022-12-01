79b777b3eeb1a4c468c5cc5ff114a17e25640fdf
Shrey Sharma <findshrey@outlook.com>
2022-12-01 19:28:04 +0530

    Refactor: contact form

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,57 +1,55 @@
 const Contact = () => (
-   <section id="contact" className="contact">
-      <div className="container">
-         <header className="head-pink">
-            <h3>Get In Touch</h3>
-         </header>
-         <div className="form-wrapper">
-            <form
-               name="contact"
-               className="form"
-               data-netlify="true"
-               method="POST"
-               // action="/"
-            >
-               {/* Hidden Input required for Netlify forms */}
+   <section id="contact" className="contact container">
+      <header className="head-pink">
+         <h3>Get In Touch</h3>
+      </header>
+      <div className="form-wrapper">
+         <form
+            name="contact"
+            className="form"
+            data-netlify="true"
+            method="POST"
+            // action="/"
+         >
+            {/* Hidden Input required for Netlify Forms */}
+            <input
+               type="hidden"
+               name="form-name"
+               value="contact"
+               netlify-honeypot="bot-field"
+            />
+            <p className="form-control">
+               <label>Name:</label>
                <input
-                  type="hidden"
-                  name="form-name"
-                  value="contact"
-                  netlify-honeypot="bot-field"
+                  name="name"
+                  type="text"
+                  placeholder="Enter your name here"
+                  required
                />
-               <div className="form-control">
-                  <label>Name:</label>
-                  <input
-                     name="name"
-                     type="text"
-                     placeholder="Enter your name here"
-                     required
-                  />
-               </div>
-               <div className="form-control">
-                  <label>Email:</label>
-                  <input
-                     name="email"
-                     type="email"
-                     placeholder="Enter your email here"
-                     required
-                  />
-               </div>
-               <div className="form-control">
-                  <label>Message:</label>
-                  <textarea
-                     name="message"
-                     cols="30"
-                     rows="10"
-                     placeholder="Enter your message here"
-                     required
-                  />
-               </div>
-               <button className="btn-pink" type="submit">
-                  Send Message
-               </button>
-            </form>
-         </div>
+            </p>
+            <p className="form-control">
+               <label>Email:</label>
+               <input
+                  name="email"
+                  type="email"
+                  placeholder="Enter your email here"
+                  required
+               />
+            </p>
+            <p className="form-control">
+               <label>Message:</label>
+               <textarea
+                  name="message"
+                  cols="30"
+                  rows="10"
+                  placeholder="Enter your message here"
+                  required
+               />
+            </p>
+            <button type="submit" className="btn-pink">
+               Send Message
+            </button>
+         </form>
       </div>
    </section>
 )
